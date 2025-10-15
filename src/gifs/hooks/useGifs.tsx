c8f8a2dc15f2cf678ea7a7c7ed7dde3c8aab7141
Mario Enrique <etalvarez03@gmail.com>
2025-10-14 23:03:24 -0600

    changing limit of searchs

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -35,8 +35,8 @@ export const useGifs = () => {
 
         // 4. Actualizar previousTerms agregando el nuevo término al inicio y limitando a 8 elementos máximo
         //  es decir no puede ser un arreglo de más de 8.
-        if (previousSearches.length >= 8) return;
-        setpreviousSearches((prev) => [query, ...prev]);
+        // if (previousSearches.length >= 8) return;
+        setpreviousSearches((prev) => [query, ...prev].slice(0,8));
 
         let apiGifs = await GetGifsByQuery(query); 
 
